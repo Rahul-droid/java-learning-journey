71ef2de9d8878002a11053f7e052794eccfcb1ba
Rahul <rahul.kr1211@gmail.com>
2026-07-01 01:10:01 +0530

    feat: to calculate the cuboid dimensions

--- src/org/example/FirstSeleniumCode.java
+++ src/org/example/FirstSeleniumCode.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSeleniumCode {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException
+    {
 
         System.out.println("System: Initializing the WebDriver...");
 
@@ -21,9 +22,20 @@ public class FirstSeleniumCode {
         // 4. Extract data from the website
         String pageTitle = driver.getTitle();
         System.out.println("Success! The page title is: " + pageTitle);
+        Thread.sleep(2000);
 
         // 5. Clean up and close the browser
         driver.quit();
         System.out.println("System: Browser closed cleanly.");
+
+        char c[] = {'A', 'B', 'C', 'D'};
+        String str = new String(c);
+        byte b[] = {65, 66,67,68};
+        String str1 = new String(b);
+
+        System.out.println(str);
+        System.out.println(str1);
+
+
     }
 }
